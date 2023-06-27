55dcfa9cf42ad5f7ec2fac7a01ec6d58278d3e73
shahab-edraak <shahab@edraaksystems.com>
2023-06-27 19:10:53 +0500

    dashboard data

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -4,7 +4,38 @@ import './Dashboard.css'
 const Dashboard = () => {
   return (
     <div className='dashboard'>
-      <h5>shahab</h5>
+      <div className='dashboard_info'>
+        <h3>Sales Activity for this week</h3>
+        <div>
+
+          <h5>Edit Layout</h5>
+          <h5>Filter by Year</h5>
+        </div>
+
+      </div>
+      <div className="cards">
+        <div className="cards_data">
+          <h5>data</h5>
+          <h3>50</h3>
+          <h6>Quality</h6>
+        </div>
+        <div className="cards_data">
+          <h5>data</h5>
+          <h3>50</h3>
+          <h6>Quality</h6>
+        </div>
+        <div className="cards_data">
+          <h5>data</h5>
+          <h3>50</h3>
+          <h6>Quality</h6>
+        </div>
+        <div className="cards_data">
+          <h5>data</h5>
+          <h3>50</h3>
+          <h6>Quality</h6>
+        </div>
+      </div>
+
     </div>
   )
 }
