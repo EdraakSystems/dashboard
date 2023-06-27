c6186e234df9a34fa61416af4b285d961143fe3d
shahab-edraak <shahab@edraaksystems.com>
2023-06-27 18:54:04 +0500

    dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import './index.css'
 
 import Navbar from './components/Navbar/Navbar'
 import Leftnav from './components/Leftnav/Leftnav'
+import Dashboard from './components/Dashboard/Dashboard'
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <Leftnav />
-    </>
+    <div className='container'>
+      <div className="topNavbar"> <Navbar /></div>
+      <div className="content">
+        <div className="sideNavbar"> <Leftnav /></div>
+        <div className="dashBoard"> <Dashboard /></div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Dashboard.css'
+
+const Dashboard = () => {
+  return (
+    <div className='dashboard'>
+      <h5>shahab</h5>
+    </div>
+  )
+}
+
+export default Dashboard
