e1ae278fdde9211f432b9dfcc855b999c02e127e
shahab-edraak <shahab@edraaksystems.com>
2023-06-27 16:55:29 +0500

    navbar desktop final

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,57 +1,53 @@
 import React from 'react'
 import './Navbar.css'
-
+import img from '../../assets/shahab.png'
 // React Icons
-import { ImFolderUpload } from 'react-icons/im'
-import { ImFolderDownload } from 'react-icons/im'
-import { BsFillCalendarRangeFill } from 'react-icons/bs'
-import { FaThList } from 'react-icons/fa'
+import { MdDriveFolderUpload } from 'react-icons/md'
+import { HiOutlineFolderDownload } from 'react-icons/hi'
+import { HiOutlinePresentationChartBar } from 'react-icons/hi'
+import { LuFolderTree } from 'react-icons/lu'
 import { BsSearch } from 'react-icons/bs'
 import { GrNotification } from 'react-icons/gr'
-import { RxAvatar } from 'react-icons/rx'
 import { RiArrowDropDownLine } from 'react-icons/ri'
 
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <h2 className="logo">Logo</h2>
-      <ul className='nav_products'>
-        <li>
-          <ImFolderUpload />
+      <h2 className="nav_logo">Logo</h2>
+      <ul className='nav_product'>
+        <li className='nav_product_list'>
+          <MdDriveFolderUpload />
           <h5>Sales Order</h5>
         </li>
-        <li>
-          <ImFolderDownload />
+        <li className='nav_product_list'>
+          <HiOutlineFolderDownload />
           <h5>Purchase Order</h5>
         </li>
-        <li>
-          <BsFillCalendarRangeFill />
-          Products
+        <li className='nav_product_list'>
+          <HiOutlinePresentationChartBar />
+          <h5>Products</h5>
         </li>
       </ul>
       <div className="nav_task">
-        <ul className='nav_task_list'>
-          <li>
-            <FaThList />
-            Task List
-          </li>
-          <li>
-            <div className='progress_parent'>
-              <div className='progress_child'></div>
-            </div>
-            <h6>Track: here comes on 33%</h6>
-          </li>
-        </ul>
+        <LuFolderTree />
+        <h5>Task List</h5>
+        <h6 className='banner'>33%</h6>
+        <div className='progress_parent'>
+          <div className='progress_child'></div>
+        </div>
+        <h5>Track: here comes on 33%</h5>
       </div>
       <div className="nav_search">
-        <RiArrowDropDownLine />
-        <p>Search</p>
+        <div>
+          <RiArrowDropDownLine />
+          <p>Search</p>
+        </div>
         <BsSearch />
       </div>
       <div className='nav_user'>
         <GrNotification />
-        <RxAvatar />
+        <img src={img} alt="profile" />
         <h6>Muhammad Hamza</h6>
         <RiArrowDropDownLine />
       </div>
