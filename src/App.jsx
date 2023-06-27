7e8b2fcaffeffb40356f7a271114a8a696553d35
shahab-edraak <shahab@edraaksystems.com>
2023-06-27 18:09:26 +0500

    leftNav final

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './index.css'
 
 import Navbar from './components/Navbar/Navbar'
+import Leftnav from './components/Leftnav/Leftnav'
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Leftnav />
     </>
   )
 }
